db6d8a0168ff8d01b5fbc1e8a14c6766338a1521
Josh Gillies <github@joshgilli.es>
2015-04-27 22:49:36 +1000

    Add simple support for asset_* keywords.

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ var mustache = require('mustache')
 mustache.tags = '% %'
 
 function render (template, data) {
-  return mustache.render(template, data)
+  var keys = Object.keys(data)
+  var length = keys.length
+  var _data = {}
+
+  for (var i = 0, key; i < length; i++) {
+    key = keys[i]
+    _data['asset_' + key] = data[key]
+  }
+
+  return mustache.render(template, _data)
 }
 
 exports.render = render
--- test.js
+++ test.js
@@ -2,6 +2,8 @@ var keyword = require('./')
 var test = require('tape')
 
 test('basic render test', function (assert) {
-  assert.equal(keyword.render('Hello, %name%', { name: 'John' }), 'Hello, John')
+  assert.equal(keyword.render('%asset_assetid%', { assetid: 42 }), '42')
+  assert.equal(keyword.render('Hello, %asset_name%', { name: 'John' }), 'Hello, John')
+  assert.equal(keyword.render('Hello, %asset_short_name%', { short_name: 'John' }), 'Hello, John')
   assert.end()
 })
