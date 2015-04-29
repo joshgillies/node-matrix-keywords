a377bcde2f2b1de848c390ec509e11d135ca5030
Josh Gillies <github@joshgilli.es>
2015-04-29 21:59:53 +1000

    Add support for name and short name linked keywords.

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ var mustache = require('mustache')
 mustache.tags = '% %'
 
 function render (template, view) {
+  var KEYWORD_REGEX = /%(asset_name_linked|asset_short_name_linked)\b/g
+
+  var _template = template.replace(KEYWORD_REGEX, '%&$1')
+  var _view = {
+    asset_name_linked: function nameLinked () {
+      if (view.name && view.href) {
+        return '<a href="' + view.href + '">' + view.name + '</a>'
+      }
+    },
+    asset_short_name_linked: function shortNameLinked () {
+      if (view.short_name && view.href) {
+        return '<a href="' + view.href + '">' + view.short_name + '</a>'
+      }
+    }
+  }
+
   var keys = Object.keys(view)
-  var _view = {}
 
   for (var i = 0, key; i < keys.length; i++) {
     key = keys[i]
     _view['asset_' + key] = view[key]
   }
 
-  return mustache.render(template, _view)
+  return mustache.render(_template, _view)
 }
 
 exports.render = render
--- test.js
+++ test.js
@@ -4,6 +4,8 @@ var test = require('tape')
 test('basic render test', function (assert) {
   assert.equal(keyword.render('%asset_assetid%', { assetid: 42 }), '42')
   assert.equal(keyword.render('Hello, %asset_name%', { name: 'John' }), 'Hello, John')
+  assert.equal(keyword.render('%asset_name_linked%', { href: '/', name: 'Home' }), '<a href="/">Home</a>')
   assert.equal(keyword.render('Hello, %asset_short_name%', { short_name: 'John' }), 'Hello, John')
+  assert.equal(keyword.render('%asset_short_name_linked%', { href: '/', short_name: 'Home' }), '<a href="/">Home</a>')
   assert.end()
 })
