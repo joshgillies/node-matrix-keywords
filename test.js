b3b4eb93ae8a35ab8277635dbc75f04d1cff1a58
Josh Gillies <github@joshgilli.es>
2015-05-18 23:12:42 +1000

    add failing tests

--- test.js
+++ test.js
@@ -9,3 +9,8 @@ test('basic render test', function (assert) {
   assert.equal(keyword.render('%asset_short_name_linked%', { href: '/', short_name: 'Home' }), '<a href="/">Home</a>')
   assert.end()
 })
+
+test('render globals', function (assert) {
+  assert.equal(keyword.render('Hello, %asset_name%. Welcome to %globals_site_name%', { global: { site: { name: 'Boom Town!' } }, name: 'John' }), 'Hello, John. Welcome to Boom Town!')
+  assert.end()
+})
