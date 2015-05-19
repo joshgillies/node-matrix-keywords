f28c9d67f4d6f4bc23faeb982d22030567051193
Josh Gillies <github@joshgilli.es>
2015-05-19 23:25:01 +1000

    Add further test, consider this a spec of sorts

--- test.js
+++ test.js
@@ -14,3 +14,13 @@ test('render globals', function (assert) {
   assert.equal(keyword.render('Hello, %asset_name%. Welcome to %globals_site_name%', { global: { site: { name: 'Boom Town!' } }, name: 'John' }), 'Hello, John. Welcome to Boom Town!')
   assert.end()
 })
+
+test('global as asset context', function (assert) {
+  assert.equal(keyword.render('Hello, %asset_name%. Have you met, %globals_asset_name:1234%?', { global: { '1234': { name: 'Jon' } }, name: 'John' }), 'Hello, John. Have you met, Jon?')
+  assert.end()
+})
+
+test('keyword modifiers', function (assert) {
+  assert.equal(keyword.render('Hello, %asset_name^uppercase%!', { name: 'John' }), 'Hello, JOHN!')
+  assert.end()
+})
